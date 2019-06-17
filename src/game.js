8653c86c10a61689f2df5f13a394cf97f3d301f8
Kirbyasdf <kirbyAugustus@gmail.com>
2019-06-17 11:56:54 -0400

    phaser import

---
+++ src/game.js
@@ -0,0 +1,10 @@
+const game  = new Phaser.game(800, 600, Phaser.AUTO, '' {
+preload: preload,
+create: create,
+update: update
+
+})
+
+function preload () {}
+function create () {}
+function update () {}
