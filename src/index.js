f0f663b5c48913df5a4a140a18411dd6b3846cd6
Kirbyasdf <kirbyAugustus@gmail.com>
2019-06-18 23:24:02 -0400

    got div to show up where we want..need it dynamic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(
-  "%c Easy Day",
-    "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)"
-  );
-  const dataURL = "http://localhost:3000/api/v1/people"
-  const defaultPic = "https://i.imgur.com/USOQOCa.png"
-  const woundCon = document.querySelector(".wound")
+  console.log("%c Easy Day",
+    "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)");
 
-  let personsArr = []
+const dataURL = "http://localhost:3000/api/v1/people"
+const defaultPic = "https://i.imgur.com/USOQOCa.png"
+const woundCon = document.querySelector(".wound")
+const medicCon = document.getElementById("medicCon")
+let personsArr = []
 
-  getWounds()
+getWounds()
 
 function getWounds(){
   fetch(dataURL)
@@ -23,15 +22,14 @@ function getWounds(){
   woundGen()
   })
 
-  function woundGen(){
+function woundGen(){
   person = personsArr[Math.floor(Math.random() * personsArr.length)]
   wound  = person.wounds[Math.floor(Math.random() * person.wounds.length)]
   woundPic =  wound.img_url
+  console.log(person)
 
-
-
-  switch("right arm"){
-    case "left arm":
+switch(wound.name){
+  case "left arm":
       injury = document.querySelector("#leftArm")
       buildWound(wound, injury)
       setWoundPic(woundPic)
@@ -61,49 +59,42 @@ function getWounds(){
       buildWound(wound, injury)
       setWoundPic(woundPic)
       break;
-    }
   }
+}
 
 function setWoundPic(woundPic){
   document.querySelector("#backdrop").innerHTML =
   `
-    <img src=${woundPic}> </img>
+    <img src=${woundPic} style="height: 500px"> </img>
   `
 }
 
 
 function buildWound(wound, injury){
-  injury.style.display = "block"
+   injury.style.display = "block"
     woundDisplay = document.createElement('div')
     woundDisplay.innerHTML =
-  `
-  <div class="modal fade" id="${injury.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-   <div class="modal-dialog" role="document">
-     <div class="modal-content">
-       <div class="modal-header">
-         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-           <span aria-hidden="true">&times;</span>
-         </button>
-       </div>
-       <div class="modal-body">
-       </div>
-       <div class="modal-footer">
-         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-         <button type="button" class="btn btn-primary">Save changes</button>
-       </div>
-     </div>
-   </div>
- </div>
 
  `
+ <div>
+   <div id="bodypart${injury.id}">
+   ${wound.name}
+   </div>
+ </div>
+ `
+  medicCon.appendChild(woundDisplay)
 
-  woundCon.appendChild(woundDisplay)
-  injury.innerHTML += `
-  <button data-toggle='modal' data-target="#${injury.id}">Check Wound</button>
+  injury.innerHTML +=
   `
+  <button id="generateForm" type="button" data-toggle='modal' data-target="#${injury.id}" >Check Wound</button>
+  `
+  document.getElementById("generateForm").onclick = function() {
+  woundDisplay.style.display = "block";
 
 }
+
+}
+
 
 // injury.
 //   .modal {
@@ -112,12 +103,11 @@ function buildWound(wound, injury){
 //   right: 100px;
 //   bottom: 0;
 //   left: 0;
-//   z-index: 10040;
+//   ;
 //   overflow: auto;
 //   overflow-y: auto;
 // }
 
-// <div class='modal' id='rightArm' tabIndex='-1' style='display:none'> Right Arm </div>
 
 
 
